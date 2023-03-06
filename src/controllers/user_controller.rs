90ff46d3c9815f25cdbafc49901238aacf0eca02
wesleyricardi <wesley14_leandro@hotmail.com>
2023-03-06 12:41:54 -0300

    controller_user first test created

--- src/controllers/user_controller.rs
+++ src/controllers/user_controller.rs
@@ -52,3 +52,35 @@ impl <M: UserModel, V: UserView> UserController for UserControllerImpl<M, V> {
         ))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{models::user_model::UserModelMock, security::jwt::JWT_ENCODE_STUB, views::user_view::UserViewMock};
+
+    use super::*;
+
+    fn get_controller_with_mocks_arg() -> UserControllerImpl<UserModelMock, UserViewMock> {
+        UserControllerImpl { 
+            model: UserModelMock,
+            view: UserViewMock,
+            jwt_encode: JWT_ENCODE_STUB
+        }
+    }
+
+    #[test]
+    fn test_register() {
+        let req = ReqRegister {
+            username: "username".to_string(),
+            email: "test@email.com".to_string(),
+            password: "password".to_string(),
+        };
+
+        let controller = get_controller_with_mocks_arg();
+        let ResRegister {user, token} = controller.register(req).unwrap().into_inner();
+
+        assert_eq!(user.clone().unwrap().id.is_empty(), false);
+        assert_eq!(user.clone().unwrap().username, "username".to_string());
+        assert_eq!(user.clone().unwrap().email, "test@email.com".to_string());
+        assert_eq!(token.is_empty(), false);
+    }
+}
