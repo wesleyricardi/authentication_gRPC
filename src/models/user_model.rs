86c5f6a2ad9adf8eb68eee82df2a1309463091e7
wesleyricardi <wesley14_leandro@hotmail.com>
2023-03-06 12:40:15 -0300

    created mock of user_model and its test for

--- src/models/user_model.rs
+++ src/models/user_model.rs
@@ -18,7 +18,7 @@ pub struct InsertUser {
 
 pub struct UserModelImpl;
 
-impl  UserModel for UserModelImpl {
+impl UserModel for UserModelImpl {
     fn insert(&self, user: InsertUser) -> Result<User, Status> {
         let  id =  Uuid::new_v4().to_string();
 
@@ -29,4 +29,40 @@ impl  UserModel for UserModelImpl {
         };
         Ok(user)
     }  
+}
+
+
+pub struct UserModelMock; 
+    impl UserModel for UserModelMock {
+        fn insert(&self, user: InsertUser) -> Result<User, Status> {
+            let  id =  "UUIDV4".to_string();
+
+            let user = User {
+                id,
+                username: user.username,
+                email: user.email,
+            };
+            Ok(user)
+        }
+    }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*; 
+
+    #[test]
+    fn test_insert() {
+        let model = UserModelImpl;
+
+        let response = model.insert(InsertUser {
+            username: "username".to_string(),
+            email: "email".to_string(),
+            password: "password".to_string(),
+        }).unwrap();
+
+        assert_eq!(response.id.is_empty(), false);
+        assert_eq!(response.username, "username".to_string());
+        assert_eq!(response.email, "email".to_string())
+    }
 }
