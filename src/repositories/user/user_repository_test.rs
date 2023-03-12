e045f1078349c12b8e12a0b8794af8e8b79b2ef9
wesleyricardi <wesley14_leandro@hotmail.com>
2023-03-12 12:30:49 -0300

    added test to store_update in user_repository

--- src/repositories/user/user_repository_test.rs
+++ src/repositories/user/user_repository_test.rs
@@ -39,4 +39,36 @@ mod tests {
         assert_eq!(response.email, "test2@email.com".to_string());
         assert_eq!(response.password, "password".to_string());
     }
+
+    #[test]
+    fn test_store_update() {
+        let user_repository = UserRepositoryMock;
+
+        user_repository.store(UserRepositoryStoreParams {
+            id: "UNIQUE_ID3".to_string(),
+            username: "username3".to_string(),
+            email: "test3@email.com".to_string(),
+            password: "password".to_string(),
+        });
+
+        user_repository
+            .store_update(
+                "UNIQUE_ID3".to_string(),
+                UserRepositoryUpdateParams {
+                    username: Some("updateduser".to_string()),
+                    email: Some("updated@email.com".to_string()),
+                    password: Some("changedpassword".to_string()),
+                },
+            )
+            .unwrap();
+
+        let response = user_repository
+            .consult_by_username("updateduser".to_string())
+            .unwrap();
+
+        assert_eq!(response.id, "UNIQUE_ID3".to_string());
+        assert_eq!(response.username, "updateduser".to_string());
+        assert_eq!(response.email, "updated@email.com".to_string());
+        assert_eq!(response.password, "changedpassword".to_string());
+    }
 }
