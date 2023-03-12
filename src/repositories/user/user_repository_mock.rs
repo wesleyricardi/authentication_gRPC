0846e6aafc05e430d50cc485de33024afd5edc96
wesleyricardi <wesley14_leandro@hotmail.com>
2023-03-12 12:24:52 -0300

    changed STORED_USERS to Mutex

--- src/repositories/user/user_repository_mock.rs
+++ src/repositories/user/user_repository_mock.rs
@@ -1,40 +1,44 @@
+use std::sync::Mutex;
+
 pub use super::user_repository::*;
 
+#[derive(Debug, Clone)]
 pub struct UserStored {
     pub id: String,
     pub username: String,
     pub email: String,
     pub password: String,
 }
-type UsersStored = Vec<UserStored>;
-static mut STORED_USERS: UsersStored = Vec::new();
+type UsersStored = Mutex<Vec<UserStored>>;
+static mut STORED_USERS: UsersStored = Mutex::new(Vec::new());
 pub struct UserRepositoryMock;
 
 impl UserRepository for UserRepositoryMock {
     fn store(&self, user: UserRepositoryStoreParams) -> UserRepositoryStoreReturn {
-        unsafe {
-            STORED_USERS.push(UserStored {
-                id: user.id,
-                username: user.username.clone(),
-                email: user.email,
-                password: user.password,
-            });
+        let stored_users = unsafe { &mut STORED_USERS.lock().unwrap() };
 
-            let result = STORED_USERS
-                .iter()
-                .find(|result| result.username == user.username)
-                .unwrap();
+        stored_users.push(UserStored {
+            id: user.id,
+            username: user.username.clone(),
+            email: user.email,
+            password: user.password,
+        });
 
-            UserRepositoryStoreReturn {
-                id: result.id.clone(),
-                username: result.username.clone(),
-                email: result.email.clone(),
-            }
+        let result = stored_users
+            .iter()
+            .find(|result| result.username == user.username)
+            .unwrap();
+
+        UserRepositoryStoreReturn {
+            id: result.id.clone(),
+            username: result.username.clone(),
+            email: result.email.clone(),
         }
     }
 
     fn consult_by_username(&self, username: String) -> Result<UserRepositoryConsultReturn, Status> {
-        let result = match unsafe { STORED_USERS.iter().find(|user| user.username == username) } {
+        let stored_user = unsafe { &mut STORED_USERS.lock().unwrap() };
+        let result = match stored_user.iter().find(|user| user.username == username) {
             Some(user) => user,
             None => return Err(Status::not_found("User not found")),
         };
