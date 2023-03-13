60a3757bde77601c53c1b3f0662cf3af8e15f8e5
wesleyricardi <wesley14_leandro@hotmail.com>
2023-03-13 09:40:21 -0300

    removed unsed functions and their tests

--- src/services/sanitizer/user_input/sanitize_user_input.rs
+++ src/services/sanitizer/user_input/sanitize_user_input.rs
@@ -1,40 +1,7 @@
 use sanitizer::prelude::*;
 pub use tonic::Status;
 
-pub struct RegisterInputDirty {
-    pub username: String,
-    pub email: String,
-    pub password: String,
-}
-
-#[derive(Sanitize, Debug)]
-pub struct RegisterInputSanitized {
-    #[sanitize(trim, alphanumeric)]
-    pub username: String,
-    #[sanitize(trim, lower_case)]
-    pub email: String,
-    #[sanitize(trim)]
-    pub password: String,
-}
-
-#[derive(Sanitize, Debug)]
-pub struct LoginInputSanitized {
-    #[sanitize(trim, alphanumeric)]
-    pub username: String,
-    #[sanitize(trim)]
-    pub password: String,
-}
-
 pub trait SanitizeUser {
-    fn register_sanitize(
-        &self,
-        user_input: RegisterInputDirty,
-    ) -> Result<RegisterInputSanitized, Status>;
-    fn login_sanitize(
-        &self,
-        username: String,
-        password: String,
-    ) -> Result<LoginInputSanitized, Status>;
     fn sanitize_username_input(&self, username: String) -> Result<String, Status>;
     fn sanitize_email_input(&self, email: String) -> Result<String, Status>;
     fn sanitize_password_input(&self, password: String) -> Result<String, Status>;
@@ -43,81 +10,6 @@ pub trait SanitizeUser {
 pub struct SanitizeUserImpl;
 
 impl SanitizeUser for SanitizeUserImpl {
-    fn register_sanitize(
-        &self,
-        user_input: RegisterInputDirty,
-    ) -> Result<RegisterInputSanitized, Status> {
-        if user_input.username.is_empty() {
-            return Err(Status::new(tonic::Code::Internal, "Username is empty"));
-        };
-        if user_input.email.is_empty() {
-            return Err(Status::new(tonic::Code::Internal, "Email is empty"));
-        };
-        if user_input.password.is_empty() {
-            return Err(Status::new(tonic::Code::Internal, "Password is empty"));
-        };
-
-        let mut instance = RegisterInputSanitized {
-            username: user_input.username,
-            email: user_input.email,
-            password: user_input.password,
-        };
-        instance.sanitize();
-
-        if instance.username.is_empty() {
-            return Err(Status::new(
-                tonic::Code::Internal,
-                "Username is empty after sanitize",
-            ));
-        };
-        if instance.email.is_empty() {
-            return Err(Status::new(
-                tonic::Code::Internal,
-                "Email is empty after sanitize",
-            ));
-        };
-        if instance.password.is_empty() {
-            return Err(Status::new(
-                tonic::Code::Internal,
-                "Password is empty after sanitize",
-            ));
-        };
-
-        return Ok(instance);
-    }
-
-    fn login_sanitize(
-        &self,
-        username: String,
-        password: String,
-    ) -> Result<LoginInputSanitized, Status> {
-        if username.is_empty() {
-            return Err(Status::new(tonic::Code::Internal, "Username is empty"));
-        };
-
-        if password.is_empty() {
-            return Err(Status::new(tonic::Code::Internal, "Password is empty"));
-        };
-
-        let mut instance = LoginInputSanitized { username, password };
-        instance.sanitize();
-
-        if instance.username.is_empty() {
-            return Err(Status::new(
-                tonic::Code::Internal,
-                "Username is empty after sanitize",
-            ));
-        };
-        if instance.password.is_empty() {
-            return Err(Status::new(
-                tonic::Code::Internal,
-                "Password is empty after sanitize",
-            ));
-        };
-
-        return Ok(instance);
-    }
-
     fn sanitize_username_input(&self, username: String) -> Result<String, Status> {
         if username.is_empty() {
             return Err(Status::new(
