b30208c1f0f1c06e40619d794d55e2702fa0e680
Wesley Ricardi <wesley14_leandro@hotmail.com>
2023-04-21 11:47:34 +0000

    add delete method and rector test_get_code

--- src/repositories/users_code/users_code_repository.rs
+++ src/repositories/users_code/users_code_repository.rs
@@ -9,6 +9,7 @@ use crate::{error::AppError, utils::adapters::sqlx_error_to_app_error::sqlx_erro
 pub trait UsersCodeRepository: Send + Sync{
     async fn store(&self, code: UsersCode) -> Result<String, AppError>;
     async fn get(&self, user_id: String, code: String) -> Result<UsersCode, AppError>;
+    async fn delete(&self, user_id: String) -> Result<String, AppError>;
 }
 
 pub struct UsersCode {
@@ -32,6 +33,12 @@ impl UsersCodeRepository for UsersCodeRepositoryPostgres<'_> {
     async fn get(&self, user_id: String, code_key: String) -> Result<UsersCode, AppError> {
         match sqlx::query_as!(UsersCode, "SELECT code, expire_at, user_id FROM users_code WHERE code = $1 and user_id = $2", code_key, user_id).fetch_one(self.pool).await {
             Ok(code) => Ok(code),
+            Err(error) => Err(sqlx_error_to_app_error(error)),
+        }
+    }
+    async fn delete(&self, user_id: String) -> Result<String, AppError> {
+        match sqlx::query!("DELETE FROM users_code WHERE user_id = $1", user_id).execute(self.pool).await {
+            Ok(_) => Ok(String::from("codes from the given user id deleted")),
             Err(error) => Err(sqlx_error_to_app_error(error)),
         }
     }
@@ -100,9 +107,9 @@ mod tests {
         let pg_url = std::env::var("POSTGRES_URL").expect("Unable to read POSTGRES_URL env var");
         let mut db_name =
             std::env::var("DATABASE_NAME").expect("Unable to read DATABASE_NAME env var");
-        db_name = format!("{db_name}_test_store_code");
+        db_name = format!("{db_name}_test_get_code");
 
-        async fn repository_store_code(pool: Pool<Postgres>) -> Result<UsersCode, AppError> {
+        async fn repository_get_code(pool: Pool<Postgres>) -> Result<UsersCode, AppError> {
             let expire: NaiveDateTime = Utc::now().naive_utc() + Duration::minutes(30);
 
             sqlx::query!(
@@ -121,15 +128,10 @@ mod tests {
                 pool: &pool
             };
     
-           match repository.get(FAKE_USER_ID.to_string(), FAKE_CODE.to_string()).await {
-            Ok(code) => {
-                Ok(code)
-            },
-            Err(error) => Err(error),
-           }
+          repository.get(FAKE_USER_ID.to_string(), FAKE_CODE.to_string()).await
         }
 
-        let response = test_with_database(pg_url, db_name, repository_store_code)
+        let response = test_with_database(pg_url, db_name, repository_get_code)
         .await
         .unwrap();
 
@@ -139,4 +141,50 @@ mod tests {
 
     }
 
+
+    #[tokio::test]
+    async fn test_delete_code() {
+        const FAKE_USER_ID: &str = "UserFakeID";
+        const FAKE_USER_USERNAME: &str = "usernames";
+        const FAKE_USER_EMAIL: &str = "test@model.com";
+        const FAKE_USER_PASSWORD: &str = "password";
+
+        const FAKE_CODE: &str = "0000001";
+      
+        dotenv::from_filename(".env.test").ok();
+        let pg_url = std::env::var("POSTGRES_URL").expect("Unable to read POSTGRES_URL env var");
+        let mut db_name =
+            std::env::var("DATABASE_NAME").expect("Unable to read DATABASE_NAME env var");
+        db_name = format!("{db_name}_test_delete_code");
+
+        async fn repository_delete_code(pool: Pool<Postgres>) -> Result<String, AppError> {
+            let expire: NaiveDateTime = Utc::now().naive_utc() + Duration::minutes(30);
+
+            sqlx::query!(
+                "INSERT INTO users (id, username, email, password) VALUES ($1, $2, $3, $4)",
+                FAKE_USER_ID,
+                FAKE_USER_USERNAME,
+                FAKE_USER_EMAIL,
+                FAKE_USER_PASSWORD, 
+            )
+            .execute(&pool)
+            .await.unwrap();
+
+            sqlx::query!("INSERT INTO users_code (code, expire_at, user_id) VALUES ($1, $2, $3)", FAKE_CODE, expire, FAKE_USER_ID).execute(&pool).await.unwrap();
+            
+            let repository = UsersCodeRepositoryPostgres {
+                pool: &pool
+            };
+    
+           repository.delete(FAKE_USER_ID.to_string()).await
+        }
+
+        let response = test_with_database(pg_url, db_name, repository_delete_code)
+        .await
+        .unwrap();
+
+
+        assert_eq!(response, "codes from the given user id deleted");
+
+    }
 }
