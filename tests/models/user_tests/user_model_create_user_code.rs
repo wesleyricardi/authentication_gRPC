5b718e2ddb561b39e74c3d952e4a9438f6c7bde5
Wesley Ricardi <wesley14_leandro@hotmail.com>
2023-04-22 16:33:15 +0000

    refactor test user_model_create_user_code

--- tests/models/user_tests/user_model_create_user_code.rs
+++ tests/models/user_tests/user_model_create_user_code.rs
@@ -1,7 +1,7 @@
-use authentication_gRPC::{repositories::users_code::users_code_repository::UsersCode, models::authentication::authentication_model::{UserModel, AuthenticationModel, CodeType}, error::{AppError, Code}};
-use chrono::{Utc};
+use authentication_gRPC::{repositories::users_code::users_code_repository::UsersCode, models::authentication::authentication_model::{AuthenticationModel, CodeType}, error::AppError};
+use chrono::Utc;
 
-use crate::mocks::{users_code_repository_mock::{MockUsersCodeRepositoryStore, MockUsersCodeRepositoryParams, get_mock_users_code_repository}, user_repository_mock::{get_mock_user_repository, MockUserRepositoryParams}};
+use crate::{mocks::{users_code_repository_mock::{MockUsersCodeRepositoryStore, MockUsersCodeRepositoryParams, get_mock_users_code_repository}}, utils::builders::UserModelBuilder};
 
 #[tokio::test]
 async fn test_user_model_create_user_code() {
@@ -14,25 +14,19 @@ async fn test_user_model_create_user_code() {
         && code.expire_at >= Utc::now().naive_utc()
     }
 
-    let mock_code_repository_expectations = MockUsersCodeRepositoryParams {
+    let mock_users_code_repository = get_mock_users_code_repository(MockUsersCodeRepositoryParams {
         store: Some(MockUsersCodeRepositoryStore {
             calls: 1,
             param_code_withf: param_code_withf,
             fn_returning: mock_code_repository_store
         }),
         ..Default::default()
-    };
-    
-    let model = UserModel {
-        user_code_repository: get_mock_users_code_repository(mock_code_repository_expectations),
-        generate_code: || FAKE_CODE.to_string(),
-        user_repository: get_mock_user_repository(MockUserRepositoryParams {
-            ..Default::default()
-        }),
-        password_hasher: mock_password_hasher_with_returning_error_if_called,
-        password_verify: mock_password_verify_with_returning_error_if_called,
-        new_id: || panic!("cannot be called on this test"),
-    };
+    });
+
+    let model = UserModelBuilder::new()
+    .mount_generate_code(|| FAKE_CODE.to_string())
+    .mount_code_repository(mock_users_code_repository)
+    .build();
 
     let code = model.create_user_code(FAKE_ID.to_string(), CodeType::Activation).await.unwrap();
 
@@ -43,21 +37,4 @@ fn mock_code_repository_store(
     _: UsersCode,
 ) -> Result<String, AppError> {
     Ok(String::from("Code store successfully"))
-}
-
-fn mock_password_verify_with_returning_error_if_called(
-    _: String,
-    _: String,
-) -> Result<bool, AppError> {
-    Err(AppError::new(
-        Code::Internal,
-        "cannot be called on this test",
-    ))
-}
-
-fn mock_password_hasher_with_returning_error_if_called(_: String) -> Result<String, AppError> {
-    Err(AppError::new(
-        Code::Internal,
-        "cannot be called on this test",
-    ))
 }
