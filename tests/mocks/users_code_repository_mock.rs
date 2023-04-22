6aca32a4e73d546d930726dee42afccd06cdcb31
Wesley Ricardi <wesley14_leandro@hotmail.com>
2023-04-22 14:00:18 +0000

    add method get to users_code_repository_mock

--- tests/mocks/users_code_repository_mock.rs
+++ tests/mocks/users_code_repository_mock.rs
@@ -1,4 +1,5 @@
 use authentication_gRPC::{repositories::users_code::users_code_repository::{UsersCode, MockUsersCodeRepository}, error::AppError};
+use mockall::predicate;
 
 pub struct MockUsersCodeRepositoryStore {
     pub calls: usize,
@@ -7,9 +8,18 @@ pub struct MockUsersCodeRepositoryStore {
         fn(code: UsersCode) -> Result<String, AppError>,
 }
 
+pub struct MockUsersCodeRepositoryGet {
+    pub calls: usize,
+    pub param_user_id_with: String,
+    pub param_code_with: String, 
+    pub fn_returning:
+        fn(user_id: String, code: String) -> Result<UsersCode, AppError>,
+}
+
 #[derive(Default)]
 pub struct MockUsersCodeRepositoryParams {
-    pub store: Option<MockUsersCodeRepositoryStore>
+    pub store: Option<MockUsersCodeRepositoryStore>,
+    pub get: Option<MockUsersCodeRepositoryGet>,
 }
 
 pub fn get_mock_users_code_repository(expectations: MockUsersCodeRepositoryParams) -> MockUsersCodeRepository {
@@ -25,5 +35,19 @@ pub fn get_mock_users_code_repository(expectations: MockUsersCodeRepositoryParam
             .returning(move |code| Box::pin(async move {fn_returning(code)}));
     }
 
+    if let Some(MockUsersCodeRepositoryGet {
+        calls, 
+        param_user_id_with, 
+        param_code_with, 
+        fn_returning
+    }) = expectations.get {
+
+        mock_users_code_repository
+            .expect_get()
+            .with(predicate::eq(param_user_id_with), predicate::eq(param_code_with))
+            .times(calls)
+            .returning(move |user_id, code| Box::pin(async move {fn_returning(user_id, code)}));
+    }
+    
     mock_users_code_repository
 }
