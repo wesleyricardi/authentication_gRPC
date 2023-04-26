21015cd8428792f0a3d44210311d080cca01511d
Wesley Ricardi <wesley14_leandro@hotmail.com>
2023-04-26 13:47:45 +0000

    add the missing methods to gRPC

--- src/rpc/authentication.rs
+++ src/rpc/authentication.rs
@@ -4,15 +4,18 @@ pub mod authentication {
 
 use authentication::authentication_server::Authentication;
 use authentication::{
-    ReqAuthentication, ReqLogin, ReqRegister, ReqUpdateUser, ResAuthentication, ResLogin,
-    ResRegister, ResUpdateUser,
+    ReqActivateUser, ReqAuthentication, ReqLogin, ReqRecoverUserPassword, ReqRegister,
+    ReqSendActivationCode, ReqSendRecoveryCode, ReqUpdatePassword, ReqUpdateUser, ResActivateUser,
+    ResAuthentication, ResLogin, ResRecoverUserPassword, ResRegister, ResSendActivationCode,
+    ResSendRecoveryCode, ResUpdatePassword, ResUpdateUser,
 };
 use tonic::{Request, Response, Status};
 
 use crate::controllers::authentication::authentication_controller::{
     AuthenticationController, LoginParams, RegisterParams, SanitizeUser, UpdateParams,
-    UserController,
+    UserController, UserControllerRecoverPasswordReq,
 };
+use crate::dtos::controllers::dtos_controller_user::UserControllerUpdatePasswordReq;
 use crate::models::authentication::authentication_model::UserModel;
 use crate::repositories::user::user_repository::UserRepositoryPostgres;
 use crate::repositories::users_code::users_code_repository::UsersCodeRepositoryPostgres;
@@ -20,8 +23,11 @@ use crate::security::jwt::{jwt_decode, jwt_encode};
 use crate::services::mail::send::send_email;
 use crate::utils::adapters::app_error_to_grpc_error::app_error_to_grpc_error;
 use crate::utils::adapters::user_controller_to_grpc_response::{
-    map_user_auth_to_grpc_response, map_user_login_to_grpc_response,
-    map_user_register_to_grpc_response, map_user_update_to_grpc_response,
+    map_recover_password_to_grpc_response, map_send_recovery_code_to_grpc_response,
+    map_user_activate_to_grpc_response, map_user_auth_to_grpc_response,
+    map_user_login_to_grpc_response, map_user_register_to_grpc_response,
+    map_user_send_activation_code_to_grpc_response, map_user_update_password_to_grpc_response,
+    map_user_update_to_grpc_response,
 };
 use crate::utils::generate_code::six_number_code_generator::six_number_code_generator;
 use crate::utils::generate_id::uuidv4::new_uuidv4;
@@ -143,6 +149,119 @@ impl Authentication for AuthenticationService {
             .await
         {
             Ok(response) => Ok(map_user_update_to_grpc_response(response)),
+            Err(error) => Err(app_error_to_grpc_error(error)),
+        }
+    }
+
+    async fn update_password(
+        &self,
+        request: Request<ReqUpdatePassword>,
+    ) -> Result<Response<ResUpdatePassword>, Status> {
+        let app_state = &self.app_state;
+        let metadata = request.metadata().to_owned();
+        let token = match metadata.get("authorization") {
+            Some(t) => t.to_str().unwrap(),
+            None => return Err(Status::unauthenticated("Token JWT not found")),
+        };
+
+        let ReqUpdatePassword {
+            new_password,
+            old_password,
+        } = request.into_inner();
+
+        let controller = create_user_controller(app_state);
+
+        match controller
+            .update_password(
+                token.to_string(),
+                UserControllerUpdatePasswordReq {
+                    new_password,
+                    old_password,
+                },
+            )
+            .await
+        {
+            Ok(response) => Ok(map_user_update_password_to_grpc_response(response)),
+            Err(error) => Err(app_error_to_grpc_error(error)),
+        }
+    }
+
+    async fn send_activation_code(
+        &self,
+        request: Request<ReqSendActivationCode>,
+    ) -> Result<Response<ResSendActivationCode>, Status> {
+        let app_state = &self.app_state;
+        let metadata = request.metadata().to_owned();
+        let token = match metadata.get("authorization") {
+            Some(t) => t.to_str().unwrap(),
+            None => return Err(Status::unauthenticated("Token JWT not found")),
+        };
+
+        let controller = create_user_controller(app_state);
+
+        match controller.send_activation_code(token.to_string()).await {
+            Ok(response) => Ok(map_user_send_activation_code_to_grpc_response(response)),
+            Err(error) => Err(app_error_to_grpc_error(error)),
+        }
+    }
+
+    async fn activate_user(
+        &self,
+        request: Request<ReqActivateUser>,
+    ) -> Result<Response<ResActivateUser>, Status> {
+        let app_state = &self.app_state;
+        let metadata = request.metadata().to_owned();
+        let token = match metadata.get("authorization") {
+            Some(t) => t.to_str().unwrap(),
+            None => return Err(Status::unauthenticated("Token JWT not found")),
+        };
+        let ReqActivateUser { code_key } = request.into_inner();
+
+        let controller = create_user_controller(app_state);
+
+        match controller.activate_user(token.to_string(), code_key).await {
+            Ok(response) => Ok(map_user_activate_to_grpc_response(response)),
+            Err(error) => Err(app_error_to_grpc_error(error)),
+        }
+    }
+
+    async fn send_recovery_code(
+        &self,
+        request: Request<ReqSendRecoveryCode>,
+    ) -> Result<Response<ResSendRecoveryCode>, Status> {
+        let app_state = &self.app_state;
+        let ReqSendRecoveryCode { email } = request.into_inner();
+
+        let controller = create_user_controller(app_state);
+
+        match controller.send_recover_code(email.to_string()).await {
+            Ok(response) => Ok(map_send_recovery_code_to_grpc_response(response)),
+            Err(error) => Err(app_error_to_grpc_error(error)),
+        }
+    }
+
+    async fn recover_user_password(
+        &self,
+        request: Request<ReqRecoverUserPassword>,
+    ) -> Result<Response<ResRecoverUserPassword>, Status> {
+        let app_state = &self.app_state;
+        let ReqRecoverUserPassword {
+            email,
+            new_password,
+            code_key,
+        } = request.into_inner();
+
+        let controller = create_user_controller(app_state);
+
+        match controller
+            .recover_user_password(UserControllerRecoverPasswordReq {
+                email,
+                new_password,
+                code_key,
+            })
+            .await
+        {
+            Ok(response) => Ok(map_recover_password_to_grpc_response(response)),
             Err(error) => Err(app_error_to_grpc_error(error)),
         }
     }
